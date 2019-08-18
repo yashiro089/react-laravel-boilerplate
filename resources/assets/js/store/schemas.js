39a714997f1a1b5abaf6e34a07c26d15d0533863
Carwyn Stephen <caztephen@gmail.com>
2019-08-18 15:19:13 +0200

    Add the setup for the posts Redux store

--- resources/assets/js/store/schemas.js
+++ resources/assets/js/store/schemas.js
@@ -1,7 +1,9 @@
 import { schema } from 'normalizr'
 
 export const user = new schema.Entity('users')
+export const post = new schema.Entity('posts')
 
 export const entities = {
-  users: [user]
+  users: [user],
+  posts: [post]
 }
